980911b6984afca1de802e75ecd4ca75ed1db72f
Alex <alexalmansa5@gmail.com>
2021-03-11 20:27:26 +0100

    Added options return 200 to make connection with frontend work

--- src/main.go
+++ src/main.go
@@ -11,6 +11,31 @@ import (
 	"strings"
 )
 
+// CORS Middleware
+func CORS(c *gin.Context) {
+
+	// First, we add the headers with need to enable CORS
+	// Make sure to adjust these headers to your needs
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Content-Type", "application/json")
+
+	// Second, we handle the OPTIONS problem
+	if c.Request.Method != "OPTIONS" {
+
+		c.Next()
+
+	} else {
+
+		// Everytime we receive an OPTIONS request,
+		// we just return an HTTP 200 Status Code
+		// Like this, Angular can now do the real
+		// request using any other method than OPTIONS
+		c.AbortWithStatus(http.StatusOK)
+	}
+}
+
 func main() {
 
 	conn, err := connectDB()
@@ -19,6 +44,7 @@ func main() {
 	}
 	Router := gin.Default()
 	Router.Use(dbMiddleware(conn))
+	Router.Use(CORS)
 	//Login endpoints
 	usersGroup := Router.Group("users")
 	{
